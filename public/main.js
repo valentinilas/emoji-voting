803f27b96c79339b7fa872d6642d4d33108e2f1e
Valentin Ilas <valentin.ilas@gmail.com>
2021-04-21 22:58:42 +0200

    added server and front-end

---
+++ public/main.js
@@ -0,0 +1,41 @@
+
+// Form submit event
+const form = document.getElementById('vote-form');
+
+form.addEventListener('submit', (event) => {
+    const choice = document.querySelector('input[name=os]:checked').value;
+    const data = { os: choice };
+
+    fetch('http://localhost:3000/poll', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(res => res.json())
+        .then(data =>  console.log(data))
+        .catch(err => console.log(err));
+
+    event.preventDefault();
+})
+
+// Print out results
+const results = document.getElementById('results');
+const showResults = (data) =>{
+    results.insertAdjacentHTML('beforeend', data);
+}
+
+// Subscribe to events
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('0c9be423f896addb0c91', {
+  cluster: 'eu'
+});
+
+var channel = pusher.subscribe('os-poll');
+channel.bind('os-vote', function(data) {
+//   alert(JSON.stringify(data));
+  showResults(data)
+});
---
+++ routes/poll.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Pusher = require("pusher");
+
+
+
+const pusher = new Pusher({
+  appId: "1192257",
+  key: "0c9be423f896addb0c91",
+  secret: "11a9412c1ac68ff55838",
+  cluster: "eu",
+  useTLS: true
+});
+
+
+
+router.get('/',(req,res)=>{
+    res.send('POLL');
+});
+router.post('/',(req, res)=>{
+    pusher.trigger("os-poll", "os-vote", {
+        points: 1,
+        os: req.body.os
+    });
+    return res.json({success: true, message: 'Thank you for voting!'});
+});
+
+module.exports = router;
