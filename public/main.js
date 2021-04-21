dd1c7006b3c49fd562ce14e953c9334c6c5c6f30
Valentin Ilas <valentin.ilas@gmail.com>
2021-04-21 23:19:59 +0200

    Added results to separate page

--- public/main.js
+++ public/main.js
@@ -20,22 +20,3 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 })
 
-// Print out results
-const results = document.getElementById('results');
-const showResults = (data) => {
-    results.insertAdjacentHTML('beforeend', data);
-}
-
-// Subscribe to events
-// Enable pusher logging - don't include this in production
-Pusher.logToConsole = true;
-
-var pusher = new Pusher('0c9be423f896addb0c91', {
-    cluster: 'eu'
-});
-
-var channel = pusher.subscribe('os-poll');
-channel.bind('os-vote', function (data) {
-    //   alert(JSON.stringify(data));
-    showResults(data)
-});
